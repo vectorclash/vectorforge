be1ab2b6dbe1bf5c64ac655384a66754c3d813e1
Aaron Sterczewski <aaron.sterczewski@gmail.com>
2020-08-31 09:18:36 -0700

    added airtable connectivity and added create function to main component

--- src/components/DisplayCanvas.js
+++ src/components/DisplayCanvas.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import createjs from 'preload-js'
+import Airtable from 'airtable'
 import './DisplayCanvas.scss'
 
 import LinearGradient from './Canvas/LinearGradient'
@@ -13,6 +14,13 @@ import s2 from '../assets/images/star-sprite-small.png'
 
 export default class DisplayCanvas extends React.Component {
   componentDidMount() {
+    Airtable.configure({
+      endpointUrl: 'https://api.airtable.com',
+      apiKey: 'keyea84FaAaGduqyx'
+    })
+
+    this.base = Airtable.base('appImg4GSWAPNn2Xc')
+
     let queueItems = [
       {id: 'star-large', src: s1},
       {id: 'star-small', src: s2}
@@ -30,13 +38,19 @@ export default class DisplayCanvas extends React.Component {
     this.canvas.width = this.props.width
     this.canvas.height = this.props.height
 
-    let gradientConfig = new GenerateLinearGradient(this.props.width, this.props.height, 1)
-    let gradientBG = new LinearGradient(gradientConfig)
-    this.context.drawImage(gradientBG, 0, 0)
+    this.mainConfig = {}
+    this.mainConfig.width = this.props.width
+    this.mainConfig.height = this.props.height
+
+    let gradientBackgroundConfig = new GenerateLinearGradient(this.props.width, this.props.height, 1)
+    this.mainConfig.gradientBackgroundConfig = gradientBackgroundConfig
+    let gradientBackground = new LinearGradient(gradientBackgroundConfig)
+    this.context.drawImage(gradientBackground, 0, 0)
 
     this.context.globalCompositeOperation = 'overlay'
 
     let radialFieldConfig = new GenerateLargeRadialField(this.props.width, this.props.height)
+    this.mainConfig.radialFieldConfig = radialFieldConfig
     let radialField = new LargeRadialField(radialFieldConfig)
     this.context.drawImage(radialField, 0, 0)
 
@@ -50,6 +64,25 @@ export default class DisplayCanvas extends React.Component {
     // this.context.drawImage(starField, 0, 0)
 
     this.canvas.toBlob(this.setImage.bind(this))
+  }
+
+  saveImageToAirtable() {
+    this.base('Images').create([
+      {
+        "fields": {
+          "ID": "NewImage" + Math.random() * 10000000,
+          "Configuration": JSON.stringify(this.mainConfig)
+        }
+      }
+    ], function(err, records) {
+      if (err) {
+        console.error(err)
+        return
+      }
+      records.forEach(function (record) {
+        console.log(record.getId())
+      })
+    })
   }
 
   setImage(blob) {
