e002b2ae2fe11b7bd956d62cce44b18520b6732a
Aaron Ezra Sterczewski <aaron.sterczewski@gmail.com>
2025-11-16 12:03:43 -0800

    adjusted button behaviour

--- src/components/buttons/SettingsButton.js
+++ src/components/buttons/SettingsButton.js
@@ -3,7 +3,34 @@ import React from 'react'
 export default class SettingsButton extends React.Component {
   render() {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M496 384H160v-16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h80v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h336c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm0-160h-80v-16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h336v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h80c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm0-160H288V48c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16C7.2 64 0 71.2 0 80v32c0 8.8 7.2 16 16 16h208v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h208c8.8 0 16-7.2 16-16V80c0-8.8-7.2-16-16-16z" /></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="settings-icon">
+        {/* Bottom slider - track */}
+        <path d="M16 400c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h480c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16H16z" />
+        {/* Bottom slider - knob */}
+        <g className="slider-knob slider-1">
+          <path d="M96 368h32c8.8 0 16 7.2 16 16v16h-64v-16c0-8.8 7.2-16 16-16z" />
+          <path d="M96 448h32c8.8 0 16 7.2 16 16v16h-64v-16c0-8.8 7.2-16 16-16z" />
+          <rect x="80" y="384" width="64" height="64" />
+        </g>
+
+        {/* Middle slider - track */}
+        <path d="M16 240c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h480c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16H16z" />
+        {/* Middle slider - knob */}
+        <g className="slider-knob slider-2">
+          <path d="M368 208h32c8.8 0 16 7.2 16 16v16h-64v-16c0-8.8 7.2-16 16-16z" />
+          <path d="M368 288h32c8.8 0 16 7.2 16 16v16h-64v-16c0-8.8 7.2-16 16-16z" />
+          <rect x="352" y="224" width="64" height="64" />
+        </g>
+
+        {/* Top slider - track */}
+        <path d="M16 80c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h480c8.8 0 16-7.2 16-16V96c0-8.8-7.2-16-16-16H16z" />
+        {/* Top slider - knob */}
+        <g className="slider-knob slider-3">
+          <path d="M240 48h32c8.8 0 16 7.2 16 16v16h-64V64c0-8.8 7.2-16 16-16z" />
+          <path d="M240 128h32c8.8 0 16 7.2 16 16v16h-64v-16c0-8.8 7.2-16 16-16z" />
+          <rect x="224" y="64" width="64" height="64" />
+        </g>
+      </svg>
     )
   }
 }
